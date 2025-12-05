1f11db82d1792a4f7ebdfda5555d1a676e052c3b
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-12-04 23:26:09 -0300

    function toggleInfo()

--- js/main.js
+++ js/main.js
@@ -107,4 +107,11 @@
     
 })(jQuery);
 
-
+function toggleInfo() {
+      const info = document.getElementById("info");
+      if (info.style.display === "none" || info.style.display === "") {
+        info.style.display = "block";
+      } else {
+        info.style.display = "none";
+      }
+    }
