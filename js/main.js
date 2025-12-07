cb01f95438e21360760870262f10c18377e02c02
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-12-07 00:36:11 -0300

    posicion tags

--- js/main.js
+++ js/main.js
@@ -108,10 +108,10 @@
 })(jQuery);
 
 function toggleInfo() {
-      const info = document.getElementById("info");
-      if (info.style.display === "none" || info.style.display === "") {
-        info.style.display = "block";
-      } else {
-        info.style.display = "none";
-      }
+    const info = document.getElementById("info");
+    if (info.style.display === "none" || info.style.display === "") {
+    info.style.display = "block";
+    } else {
+    info.style.display = "none";
     }
+}
